0983796d507cd2399421cae79e9ec8a300af49d4
raiyan1999j <62192284+raiyan1999j@users.noreply.github.com>
2024-06-19 15:57:36 +0600

    custom per page limitation scholar data

--- src/Component/AllScholars/AllScholars.jsx
+++ src/Component/AllScholars/AllScholars.jsx
@@ -1,29 +1,44 @@
 import { IoSearchOutline } from "react-icons/io5";
 import ScholarCard from "./ScholarCard";
 import ScholarPagination from "./ScholarPagination";
-import { useEffect,useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { publicRoute } from "../../PublicRoute/PublicRoute";
+import { FaPlus } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import { limit } from "firebase/firestore";
+
+const optArray = [4,8,12];
 
 export default function AllScholars() {
-    const [pagination,setPage] = useState(1);
-    const [totalPage,setTotal] = useState(1);
-    const [allInfo,setInfo] = useState(null);
-    const pageArray = [];
-    useEffect(()=>{
-        publicRoute(`/allScholarData?pageNumber=${pagination}`)
-        .then((res)=>{
-            setTotal(res.data.totalPage)
-            setInfo(res.data.result)
-        })
-    },[pagination])
+  const [pagination, setPage] = useState(1);
+  const [totalPage, setTotal] = useState(1);
+  const [limitation,setLimit] = useState(4);
+  const [allInfo, setInfo] = useState(null);
+  const [condition,setCondition] = useState(true);
+  const customPageNum = useRef();
+  const limitPerPage = useRef(4);
+  const pageArray = [];
+  useEffect(() => {
+    publicRoute(`/allScholarData?pageNumber=${pagination}&&limitation=${limitation}`).then((res) => {
+      setTotal(res.data.totalPage);
+      setInfo(res.data.result);
+    });
+  }, [pagination,limitation]);
 
-    const setPageNumber=(value)=>{
-      setPage(value)
-    }
+  const setPageNumber = (value) => {
+    setPage(value);
+  };
 
-    for(let repeat=1;repeat<=totalPage; repeat++){
-      pageArray.push(repeat);
-    }
+  const addToOpt=()=>{
+    setCondition(true);
+    const pageNum = customPageNum.current.value;
+
+    pageNum== "" ? 4 : Number(optArray.push(pageNum));
+  }
+
+  for (let repeat = 1; repeat <= totalPage; repeat++) {
+    pageArray.push(repeat);
+  }
   return (
     <>
       <section className="bg-dashNav bg-no-repeat bg-cover">
@@ -43,25 +58,50 @@ export default function AllScholars() {
         </section>
 
         <section className="w-[1200px] mx-auto mt-[100px]">
-          {/* <ScholarCard /> */}
-          {allInfo?.map((value,index)=>{
-            return <ScholarCard allData={value} key={index}/>
+          {allInfo?.map((value, index) => {
+            return <ScholarCard allData={value} key={index} />;
           })}
         </section>
 
-        <section className="w-[1200px] mx-auto flex justify-end">
-          {
-            pageArray.map((value,index)=>{
-              return(
-                <ScholarPagination 
-                key={index} 
-                pageNumber={value}
-                paginationNumber={pagination}
-                pageNumberSet={(value)=>{setPageNumber(value)}}
+        <section className="w-[1200px] mx-auto grid grid-cols-[50%_50%] justify-between">
+        <div className="flex flex-row items-center">
+            <h4 className="capitalize font-bold text-blue-950 font-serif">show</h4>
+            {
+              condition?
+              <select className="w-[10%] py-1 px-2 rounded-xl max-w-xs bg-black text-white ml-3" ref={limitPerPage} onChange={()=>{setLimit(limitPerPage.current.value)}}>
+              {
+                optArray.map((value,index)=>{
+                  return <option value={value} key={index}>{value}</option>
+                })
+              }
+            </select>:
+            <input type="number" className="w-[10%] border border-black rounded-md ml-3 font-bold font-mono " ref={customPageNum}/>
+            } 
+            {
+              condition?
+              <button className="py-2 px-2 rounded-xl shadow-inner shadow-black bg-white/50 ml-3 transition-all duration-500 hover:bg-slate-800 hover:text-white hover:shadow-xl hover:shadow-gray-600" onClick={()=>{setCondition(false)}}>
+                <FaPlus className="transition-all duration-100"/>
+              </button>:
+              <button className="py-2 px-2 rounded-xl shadow-inner shadow-black bg-white/50 ml-3 transition-all duration-500 hover:bg-slate-800 hover:text-white hover:shadow-xl hover:shadow-gray-600" onClick={()=>{addToOpt()}}>
+                <ImCross className="transition-all duration-100"/>
+              </button>
+            }
+            <h4 className="capitalize font-bold text-blue-950 font-serif">per page</h4>
+          </div>
+          <div className="text-right">
+            {pageArray.map((value, index) => {
+              return (
+                <ScholarPagination
+                  key={index}
+                  pageNumber={value}
+                  paginationNumber={pagination}
+                  pageNumberSet={(value) => {
+                    setPageNumber(value);
+                  }}
                 />
-              )
-            })
-          }
+              );
+            })}
+          </div>
         </section>
       </section>
     </>
--- src/Component/AllScholars/ScholarCard.jsx
+++ src/Component/AllScholars/ScholarCard.jsx
@@ -1,53 +1,86 @@
 import { BiBadgeCheck } from "react-icons/bi";
-export default function ScholarCard({allData}){
-    const {university,scholarshipName,country,city,subject,deadline,service,photo} = allData;
-    return(
-        <>
-            <div className="rounded-md shadow-md shadow-gray-400 py-2 px-2 w-[80%] grid grid-cols-[20%_77%] gap-x-4 mb-10">
-                <div className="p-4 w-full h-[300px]">
-                    <img src={photo} alt="coverPhoto" className="h-full w-full object-cover rounded-xl" />
-                </div>
-                <div>
-                    <h2 className="text-blue-950 font-bold font-serif text-2xl capitalize">
-                        {university}
-                    </h2>
-                    <div className="my-2">
-                        <div className="flex flex-row mb-3">
-                            <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-rose-500 before:table before:clear-both before:mr-2 flex items-center">location:</h2>
-                            <p className="ml-4 font-mono text-base font-medium capitalize italic">{country},{city}</p>
-                        </div>
-                        <div className="flex flex-row mb-3">
-                            <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-green-500 before:table before:clear-both before:mr-2 flex items-center">scholarship category:</h2>
-                            <p className="ml-4 font-mono text-base font-medium capitalize italic">{scholarshipName}</p>
-                        </div>
-                        <div className="flex flex-row mb-3">
-                            <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-yellow-500 before:table before:clear-both before:mr-2 flex items-center">subject:</h2>
-                            <p className="ml-4 font-mono text-base font-medium capitalize italic">{subject}</p>
-                        </div>
-                        <div className="flex flex-row mb-3">
-                            <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-orange-500 before:table before:clear-both before:mr-2 flex items-center">application deadline</h2>
-                            <p className="ml-4 font-mono text-base font-medium capitalize italic">{deadline}</p>
-                        </div>
-                        <div className="flex flex-row mb-3">
-                            <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-pink-500 before:table before:clear-both before:mr-2 flex items-center">application fees</h2>
-                            <p className="ml-4 font-mono text-base font-medium capitalize italic">{service}</p>
-                        </div>
-                        <div className="flex flex-row justify-between w-[100%] mx-auto">
-                        <div className="flex flex-row items-center">
-                            <span className="mr-2">
-                            <BiBadgeCheck/>
-                            </span>
-                            <span className="capitalize text-bold font-serif text-indigo-700">rating</span>
-                        </div>
-                        <div>
-                            <button className="scholarsBtn">
-                                Details
-                            </button>
-                        </div>
-                        </div>
-                    </div>
-                </div>
+export default function ScholarCard({ allData }) {
+  const {
+    university,
+    scholarshipName,
+    country,
+    city,
+    subject,
+    deadline,
+    service,
+    photo,
+  } = allData;
+  return (
+    <>
+      <div className="rounded-md shadow-md shadow-gray-400 py-2 px-2 w-[80%] grid grid-cols-[20%_77%] gap-x-4 mb-10">
+        <div className="p-4 w-full h-[300px]">
+          <img
+            src={photo}
+            alt="coverPhoto"
+            className="h-full w-full object-cover rounded-xl"
+          />
+        </div>
+        <div>
+          <h2 className="text-blue-950 font-bold font-serif text-2xl capitalize">
+            {university}
+          </h2>
+          <div className="my-2">
+            <div className="flex flex-row mb-3">
+              <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-rose-500 before:table before:clear-both before:mr-2 flex items-center">
+                location:
+              </h2>
+              <p className="ml-4 font-mono text-base font-medium capitalize italic">
+                {country},{city}
+              </p>
             </div>
-        </>
-    )
+            <div className="flex flex-row mb-3">
+              <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-green-500 before:table before:clear-both before:mr-2 flex items-center">
+                scholarship category:
+              </h2>
+              <p className="ml-4 font-mono text-base font-medium capitalize italic">
+                {scholarshipName}
+              </p>
+            </div>
+            <div className="flex flex-row mb-3">
+              <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-yellow-500 before:table before:clear-both before:mr-2 flex items-center">
+                subject:
+              </h2>
+              <p className="ml-4 font-mono text-base font-medium capitalize italic">
+                {subject}
+              </p>
+            </div>
+            <div className="flex flex-row mb-3">
+              <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-orange-500 before:table before:clear-both before:mr-2 flex items-center">
+                application deadline
+              </h2>
+              <p className="ml-4 font-mono text-base font-medium capitalize italic">
+                {deadline}
+              </p>
+            </div>
+            <div className="flex flex-row mb-3">
+              <h2 className="text-base font-bold text-blue-400 capitalize before:h-2 before:w-2 before:rounded-full before:bg-pink-500 before:table before:clear-both before:mr-2 flex items-center">
+                application fees
+              </h2>
+              <p className="ml-4 font-mono text-base font-medium capitalize italic">
+                {service}
+              </p>
+            </div>
+            <div className="flex flex-row justify-between w-[100%] mx-auto">
+              <div className="flex flex-row items-center">
+                <span className="mr-2">
+                  <BiBadgeCheck />
+                </span>
+                <span className="capitalize text-bold font-serif text-indigo-700">
+                  rating
+                </span>
+              </div>
+              <div>
+                <button className="scholarsBtn">Details</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
