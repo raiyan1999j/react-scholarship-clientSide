15d7e68a5aa6e9e11f5525626181dd7da455b75b
raiyan1999j <62192284+raiyan1999j@users.noreply.github.com>
2024-07-06 10:23:27 +0600

    check whether already paid or not

--- src/Component/Payment/Payment.jsx
+++ src/Component/Payment/Payment.jsx
@@ -1,18 +1,69 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom"
 import PaymentBox from "./PaymentBox";
 import AppForm from "./AppForm";
+import { InfoContainer } from "../../AuthProvider/AuthProvider";
+import { publicRoute } from "../../PublicRoute/PublicRoute";
+import { Flip, Slide, toast } from "react-toastify";
 
 export default function Payment(){
     const [condition,setCondition] = useState(true);
     const {application} = useLoaderData();
     const loader = useLoaderData();
+    const {user} = useContext(InfoContainer);
+
+    const paymentOpt=(value)=>{
+        setCondition(value);
+
+        const wrap ={
+            email: user.email,
+            track: loader._id,
+            amount: application
+        }
+
+        publicRoute.post('/paymentData',wrap)
+        .then((res)=>{
+            if(res.status == 200){
+                toast.success('Payment Done', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                    transition: Slide,
+                    });
+            }
+        })
+    }
+
+    useEffect(()=>{
+        publicRoute(`/paymentCheck?email=${user.email}&&track=${loader._id}`)
+        .then((res)=>{
+            if(!res.data){
+                setCondition(false)
+                toast.info('Already paid', {
+                    position: "top-left",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                    transition: Flip,
+                    });
+            }
+        })
+    },[])
     return(
         <>
             <section className="w-[1200px] mx-auto mt-[100px]">
                 {
                     condition?
-                    <PaymentBox fees={application} conditionChange={(value)=>{setCondition(value)}}/>
+                    <PaymentBox fees={application} conditionChange={(value)=>{paymentOpt(value)}}/>
                     :
                     <AppForm preDefined={loader}/>
                 }
--- src/Component/Payment/PaymentBox.jsx
+++ src/Component/Payment/PaymentBox.jsx
@@ -1,25 +1,32 @@
-
-export default function PaymentBox({fees,conditionChange}){
-    return(
-        <>
-            <div className="w-[50%] mx-auto shadow-lg shadow-black rounded-xl pb-4">
-                        <div className="py-4 bg-success rounded-t-xl">
-                            <h2 className="capitalize font-serif text-bold text-xl text-white text-center">
-                                Pay for your fees
-                            </h2>
-                        </div>
-                        <div className="my-4 flex flex-row w-[90%] mx-auto">
-                        <p className="mr-4 py-1 font-sans text-xl font-bold text-slate-800">Total amount:</p>
-                        <input type="text" value={fees} className="py-2 pl-3 bg-white hover:cursor-not-allowed" disabled />
-                        </div>
-                        <div className="w-[90%] mx-auto mt-9" onClick={()=>{
-                            conditionChange(false)
-                        }}>
-                            <button className="scholarsBtn">
-                                Pay the Fees
-                            </button>
-                        </div>
-                    </div>
-        </>
-    )
+export default function PaymentBox({ fees, conditionChange }) {
+  return (
+    <>
+      <div className="w-[50%] mx-auto shadow-lg shadow-black rounded-xl pb-4">
+        <div className="py-4 bg-success rounded-t-xl">
+          <h2 className="capitalize font-serif text-bold text-xl text-white text-center">
+            Pay for your fees
+          </h2>
+        </div>
+        <div className="my-4 flex flex-row w-[90%] mx-auto">
+          <p className="mr-4 py-1 font-sans text-xl font-bold text-slate-800">
+            Total amount:
+          </p>
+          <input
+            type="text"
+            value={fees}
+            className="py-2 pl-3 bg-white hover:cursor-not-allowed"
+            disabled
+          />
+        </div>
+        <div
+          className="w-[90%] mx-auto mt-9"
+          
+        >
+          <button className="scholarsBtn" onClick={() => {
+            conditionChange(false);
+          }}>Pay the Fees</button>
+        </div>
+      </div>
+    </>
+  );
 }
